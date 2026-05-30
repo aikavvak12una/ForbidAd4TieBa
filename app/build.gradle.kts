cc6a0b9037a15303d5244b0079240afbbc82c18b
aikavvak12una <aikavvak12una@github.com>
2026-05-30 20:08:20 +0800

    修复吧名、用户名组件缺失

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = moduleVersionCode
-        versionName = "26052902"
+        versionName = "26053001"
         buildConfigField(
             "int",
             "MIN_SUPPORTED_USER_SETTINGS_VERSION_CODE",
