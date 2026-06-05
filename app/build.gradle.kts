a1042fa91b5627e159920937605b2cbd31efbbd1
aikavvak12una <aikavvak12una@github.com>
2026-06-06 01:58:18 +0800

    更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = moduleVersionCode
-        versionName = "26060501"
+        versionName = "26060606"
         buildConfigField(
             "int",
             "MIN_SUPPORTED_USER_SETTINGS_VERSION_CODE",
